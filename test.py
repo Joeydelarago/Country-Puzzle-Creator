a4199d25f2169fd4fff1090156fdb14332d02041
Joey de l'Arago <joeydelarago@gmail.com>
2022-11-20 22:06:22 +0100

    Uncomment create models

--- test.py
+++ test.py
@@ -5,11 +5,13 @@ from mesh_generating_utils import create_mesh
 
 def test_run():
     print("aosntusonth")
-# def create_models(image_path: str = "test.png", output_path: str = "", invert: bool = True, flat: bool = True, max_height: int = 255, backplate: bool = False) -> None:
-#     pixels_mesh: Trimesh = create_mesh(image_path, invert, flat, max_height, False)
-#     pixels_mesh.export(output_path)
+    create_models()
+    
+def create_models(image_path: str = "test.png", output_path: str = "", invert: bool = True, flat: bool = True, max_height: int = 255, backplate: bool = False) -> None:
+    pixels_mesh: Trimesh = create_mesh(image_path, invert, flat, max_height, False)
+    pixels_mesh.export(output_path)
 
-#     if backplate:
-#         backplate_mesh: Trimesh = create_mesh(image_path, False, True, 1, True)
-#         backplate_mesh.export("backplate.stl")
+    if backplate:
+        backplate_mesh: Trimesh = create_mesh(image_path, False, True, 1, True)
+        backplate_mesh.export("backplate.stl")
 
